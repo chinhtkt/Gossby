4fc3698710224046bfd8eea14595aa9d94fbfbee
chinhtkt <winchesty@yahoo.com.vn>
2021-07-14 16:48:36 +0700

    updated

--- js/index.js
+++ js/index.js
@@ -1,8 +1,12 @@
-bulmaCarousel.attach('#carousel-demo', {
-    slidesToScroll: 1,
-    slidesToShow: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    loop: true,
+bulmaCarousel.attach("#carousel-demo", {
+  slidesToScroll: 1,
+  slidesToShow: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  loop: true,
+  breakpoints: [
+    { changePoint: 480, slidesToShow: 1, slidesToScroll: 1 },
+    { changePoint: 640, slidesToShow: 1, slidesToScroll: 1 },
+    { changePoint: 768, slidesToShow: 1, slidesToScroll: 1 },
+  ],
 });
-
